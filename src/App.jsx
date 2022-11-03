259ce35950aa0571015933c45846438c23ef2c3a
Mixe101 <ejbolts@hotmail.com>
2022-11-04 08:37:48 +1000

    Added darkmode button, Summary section, background layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import DarkModeBtn from "./components/DarkModeBtn";
 import Introdcution from "./components/Introduction";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App font-inter">
-      <Introdcution />
+    <div className="fixed inset-0 flex justify-center sm:px-8 bg-white dark:bg-black duration-300">
+      <div className="flex w-full max-w-7xl lg:px-8 ">
+        <div className="w-full bg-zinc-200 ring-1 ring-zinc-400 dark:bg-zinc-900 dark:ring-zinc-700  duration-300">
+          <div className="font-inter">
+            <DarkModeBtn />
+            <Introdcution />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
