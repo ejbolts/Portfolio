f7415ccf4bac17e10c5e55204a2e8922fb02af28
Mixe101 <ejbolts@hotmail.com>
2022-11-04 18:23:23 +1000

    updated border

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -15,8 +15,8 @@ const Introdcution = () => {
                 alt="Modern building architecture"
               />
             </div>
-            <div className="p-8 md:rounded-r-lg max-sm:rounded-b-lg md:border-y-2 border-r-2 max-sm:border-x-2 sm:border-x-2 border-b-2 border-white  text-gray-600  duration-300">
-              <h2 className="block mt-1 text-lg leading-tight font-bold   dark:text-white  duration-300">
+            <div className="p-8 md:rounded-r-lg max-sm:rounded-b-lg md:border-y-2 border-r-2 max-sm:border-x-2 sm:border-x-2 border-b-2 border-white  dark:border-zinc-700 text-gray-600  duration-300">
+              <h2 className="block mt-1 text-lg leading-tight font-bold  text-black dark:text-white  duration-300">
                 Summary
               </h2>
               <p className="mt-2 text-gray-600 dark:text-gray-300  text-left  duration-300">
