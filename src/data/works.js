9ac28db03f70de93c73f240e80363bc26250e939
ebolts <ejbolts@hotmail.com>
2023-01-28 10:00:34 +1000

    Updated project info

--- src/data/works.js
+++ src/data/works.js
@@ -1,12 +1,13 @@
 export default [
   {
     title: "Web Mp3 player",
-    imgUrl: "./images/web-mp3-pic.png",
+    imgUrl:
+      "https://github.com/ebolts/Portfolio/blob/main/src/images/web-mp3-pic.png?raw=true",
     tech: ["HTML", "CSS", "Javascript", "IndexedDB"],
     demoUrl: "https://ebolts.github.io/Web-mp3-player",
     workUrl: "https://github.com/ebolts/Web-mp3-player",
     about:
-      "Simple to-do list application running in android studio that allows users to create, update, and delete to-do's.",
+      "Web Mp3 player that can search for songs by their name or artist, select from a genre, and can upload your own songs all while saving them to your library or playlists.",
   },
   {
     title: "To-do App",
